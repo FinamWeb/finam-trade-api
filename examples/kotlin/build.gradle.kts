49a04546b93544d067e5eb1320629078fec2aab7
aparfenov <aparfenov@corp.finam.ru>
2025-08-25 18:59:47 +0300

    workflow build proto - update 3

--- examples/kotlin/build.gradle.kts
+++ examples/kotlin/build.gradle.kts
@@ -1,16 +1,19 @@
-import java.net.URI
-
 plugins {
     id("java")
     kotlin("jvm") version "2.2.0"
     id("com.google.protobuf") version "0.9.5"
-    id("maven-publish")
+}
+
+sourceSets.main {
+    java.srcDir("../../sdk/kotlin/src/main")
 }
 
 val coroutinesVersion: String by project
 val protobufVersion: String by project
 val grpcVersion: String by project
 val grpcKotlinVersion: String by project
+val junitJupiterVersion: String by project
+val junitPlatformLauncherVersion: String by project
 
 repositories {
     mavenCentral()
@@ -20,7 +23,7 @@ dependencies {
     // Kotlin Coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
 
-    // Protobuf
+//    // Protobuf
     implementation("com.google.protobuf:protobuf-java:$protobufVersion")
     implementation("com.google.protobuf:protobuf-kotlin:$protobufVersion")
     implementation("com.google.protobuf:protobuf-java-util:$protobufVersion")
@@ -35,6 +38,11 @@ dependencies {
     // gRPC Kotlin
     implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("org.junit.platform:junit-platform-launcher:${junitPlatformLauncherVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:${junitJupiterVersion}")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${junitJupiterVersion}")
+
 }
 
 kotlin {
@@ -69,15 +77,9 @@ protobuf {
     }
 }
 
-publishing {
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = URI("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = System.getenv("MAVEN_USERNAME")
-                password = System.getenv("MAVEN_PASSWORD")
-            }
-        }
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        showStandardStreams = true
     }
 }
